6fa9ab3d79fee9e04bff3ce6f5d9e9145ed6f8f7
WATANAPEI <info@wpei.dev>
2020-11-07 17:30:29 +0900

    styled header

--- traffik/src/components/Header.js
+++ traffik/src/components/Header.js
@@ -1,17 +1,36 @@
 import React from "react";
+import styled from "styled-components";
 
+const HeaderContainer = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: baseline;
+`;
+
+const HeaderLogo = styled.header`
+    font-size: 2em;
+`;
+
+const HeaderMenu = styled.ul`
+    list-style: none;
+    display: flex;
+    justify-content: space-between;
+`;
+
+const HeaderMenuItem = styled.li`
+    font-size: 1.5em;
+    padding: 0 1em;
+`;
 
 export default function Header() {
     return (
-        <div className="Header">
-            <div className="Header_logo">
-                Header Logo
-            </div>
-            <ul className="Header_Menu">
-                <li className="Header_MenuItem">item1</li>
-                <li className="Header_MenuItem">item2</li>
-                <li className="Header_MenuItem">item3</li>
-            </ul>
-        </div>
+        <HeaderContainer>
+            <HeaderLogo>Here Header Logo</HeaderLogo>
+            <HeaderMenu>
+                <HeaderMenuItem>Item1</HeaderMenuItem>
+                <HeaderMenuItem>Item2</HeaderMenuItem>
+                <HeaderMenuItem>Item3</HeaderMenuItem>
+            </HeaderMenu>
+        </HeaderContainer>
     )
 }
