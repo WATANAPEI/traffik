3aa186cae51e945442cbfe07512ac9055f27d1a4
WATANAPEI <info@wpei.dev>
2020-11-09 22:53:01 +0900

    add svg fundamentals

--- traffik/src/components/Summary.tsx
+++ traffik/src/components/Summary.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import styled from "styled-components";
 
 const SummaryContainer = styled.div`
-    width: 80vw;
-    height: 70%;
+    width: 800px;
+    height: 600px;
     margin: 10px auto;
     background-color: #ccc;
     padding: 10px;
@@ -12,6 +12,12 @@ const SummaryContainer = styled.div`
 export default function Summary() {
     return (
         <SummaryContainer>
+            <svg viewBox="0 0 800 600">
+                <g fill="#c33" stroke="#822" stroke-width="2">
+                    <circle cx="60" cy="60" r="50" />
+                    <circle cx="200" cy="200" r="50" />
+                </g>
+            </svg>
         </SummaryContainer>
     );
 }
