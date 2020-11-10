c1a3949544773c5c048786cbe68a020f83b037e9
WATANAPEI <info@wpei.dev>
2020-11-11 00:40:07 +0900

    add styled component multiple class like controll

--- traffik/src/components/Summary.tsx
+++ traffik/src/components/Summary.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 
+type CircleType = "domestic" | "isp";
+
 const SummaryContainer = styled.div`
     width: 800px;
     height: 600px;
@@ -9,13 +11,23 @@ const SummaryContainer = styled.div`
     padding: 10px;
 `;
 
+const Circle = styled.circle<{type: CircleType}>`
+    fill: ${
+        ({type}) => type === "domestic" ?
+            `rgba(100, 200, 140, 1)` :
+            `rgba(200, 10, 100, 1)`
+    };
+    stroke: none;
+`;
+
+
 export default function Summary() {
     return (
         <SummaryContainer>
             <svg viewBox="0 0 800 600">
                 <g fill="#c33" stroke="#822" stroke-width="2">
-                    <circle cx="60" cy="60" r="50" />
-                    <circle cx="200" cy="200" r="50" />
+                    <Circle type="domestic" id="circle_domestic" cx="60" cy="60" r="50" />
+                    <Circle type="isp" cx="200" cy="200" r="50" />
                 </g>
             </svg>
         </SummaryContainer>
