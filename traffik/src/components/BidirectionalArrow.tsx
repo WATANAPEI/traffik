2726d28949b95d882b991b8ed66ae93927c9ff54
WATANAPEI <info@wpei.dev>
2020-11-19 14:17:43 +0000

    test commit

--- traffik/src/components/BidirectionalArrow.tsx
+++ traffik/src/components/BidirectionalArrow.tsx
@@ -9,17 +9,6 @@ type parms = {
     text: string | null
 }
 
-const customStyles = {
-    content: {
-        top: "50%",
-        left: "50%",
-        right: "auto",
-        bottom: "auto",
-        marginRight: "-50%",
-        transform: "translate(-50%, -50%)"
-    }
-};
-
 const Arrow = styled.div<{degree: number}>`
     & {
         display: inline-block;
